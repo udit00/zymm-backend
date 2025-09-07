cffc03356dd535354bb132296107b0d62892dc28
udit-nair <uditnair90@gmail.com>
2025-09-07 14:58:09 +0530

    initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"zymm/internal/api"
+
+	"github.com/joho/godotenv"
+)
+
+/*
+	go run ./cmd/server/main.go
+
+*/
+
+func main() {
+	// load variables from .env into the environment
+	if err := godotenv.Load(); err != nil {
+		log.Println("⚠️  No .env file found, falling back to system env")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("❌ PORT not set in environment. Exiting...")
+	}
+
+	mux := http.NewServeMux()
+
+	/*
+	 register routes
+	 http://localhost:10000/<handler>
+	 ex: http://localhost:10000/api/
+	*/
+
+	mux.HandleFunc("/health", api.TestHandler)
+
+	log.Println("🚀 Server running on http://localhost:" + port)
+	err := http.ListenAndServe(":"+port, mux)
+	if err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
+}
---
+++ internal/api/user_handler.go
@@ -0,0 +1,12 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func TestHandler(w http.ResponseWriter, r *http.Request) {
+	response := map[string]string{"status": "ok"}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
